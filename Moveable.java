36cf451a13cc28bc53666ace80b4ec0a77523422
matthalvorson33 <matt.halvorson33@gmail.com>
2017-05-17 17:16:52 -0700

    added Moveable interface

---
+++ Moveable.java
@@ -0,0 +1,10 @@
+//This interface is for all objects that can move
+
+public interface Moveable {
+   /**
+    * Allows for movement
+    * @Param x Delta for x position
+    * @Param y Delta for y position
+    */
+   public void move(int x, int y);
+}
