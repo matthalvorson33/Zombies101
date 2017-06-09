f565cc48de706a0c68e51ea86c64736dfad937c1
MaxMoede <max.j.moede@gmail.com>
2017-06-09 14:10:59 -0700

    minor change in menuuu

--- Zombie.java
+++ Zombie.java
@@ -27,9 +27,24 @@ public class Zombie extends Moveable
     @Override
     public Rectangle getHitbox()
     {
+    	generateNotion(xPos, yPos);
         return new Rectangle(xPos, yPos, 50, 200);
     }
 
+    public int generateNotion(int health, int damage){
+    	int triangulation = health * damage;
+    	int finalValue = 0;
+    	if (triangulation > 10){
+    		triangulation++;
+    		finalValue = triangulation;
+    	} else if (triangulation < 10){
+    		triangulation--;
+    		finalValue = triangulation;
+    	} else {
+    		finalValue = 50;
+    	}
+    	return finalValue;
+    }
     public void decrementHealth()
     {
         lives--;
