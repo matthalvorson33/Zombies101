f0c547084b7b49aaaf52ae25a86ffd70937623a1
jcheruve <jcheruve@calpoly.edu>
2017-06-09 13:14:34 -0700

    small change to diff

--- DifficultyChange.java
+++ DifficultyChange.java
@@ -14,8 +14,6 @@ import java.awt.Button;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
