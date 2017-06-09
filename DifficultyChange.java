f9687f99e3f05c9ed5f0e58e41adfa58b90923b0
jcheruve <jcheruve@calpoly.edu>
2017-06-09 12:35:26 -0700

    smells on change difficulty

--- DifficultyChange.java
+++ DifficultyChange.java
@@ -22,32 +22,30 @@ import java.io.File;
 import java.io.IOException;
 @SuppressWarnings("unused")
 public class DifficultyChange extends JPanel {
-	private int width;
-	private int height;
 	public static final int SCALE = 3;
 	public static final String NAME = "Zombies101";
 	private BufferedImage img;
-	protected backgroundPanel container;
+	protected BackgroundPanel container;
 	public boolean playGame = false;
 	public boolean clicked;
 	public String difficulty;
 	public DifficultyChange(){
-		JPanel panel1 = new JPanel();
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 	}
 	
 	public String getDifficulty() {
 		return difficulty;
 	}
+	
+	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		Graphics2D g2d = (Graphics2D) g;
 		g.drawImage(img, 0, 0, this);
 	}
 	
 	public void createDfficulty()
 	{
-		container = new backgroundPanel();
+		container = new BackgroundPanel();
 		container.setAlignmentX(CENTER_ALIGNMENT);
 		container.setAlignmentY(CENTER_ALIGNMENT);
 		container.setPreferredSize(new Dimension(640, 480));
@@ -62,7 +60,6 @@ public class DifficultyChange extends JPanel {
 		try {
 			img = ImageIO.read(new File("images/difficulty.png"));
 		}catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
@@ -72,40 +69,58 @@ public class DifficultyChange extends JPanel {
 		 easy.setAlignmentY(CENTER_ALIGNMENT);
 		 easy.setPreferredSize(new Dimension(100, 150));
 
-		 easy.addActionListener(new ActionListener() {
+		 easy.addActionListener(e -> {
+			 difficulty = "easy";
+			 clicked = true;
+		 });
+		 
+		 /*easy.addActionListener(new ActionListener() {
+			 @Override
 		      public void actionPerformed(ActionEvent e)
 		      {
 		        // display/center the jdialog when the button is pressed
 		        difficulty = "easy";
 		        clicked = true;
 		      }
-		    });
+		    });*/
 		 JRadioButton medium = new JRadioButton("medium");
 		 medium.setAlignmentX(CENTER_ALIGNMENT);
 		 medium.setAlignmentY(CENTER_ALIGNMENT);
 		 medium.setPreferredSize(new Dimension(100, 150));
 
-		 medium.addActionListener(new ActionListener() {
+		 medium.addActionListener(e -> {
+			 difficulty = "medium";
+			 clicked = true;
+		 });
+		 
+		 /*medium.addActionListener(new ActionListener() {
+			 @Override
 		      public void actionPerformed(ActionEvent e)
 		      {
 		        // display/center the jdialog when the button is pressed
 		        difficulty = "medium";
 		        clicked = true;
 		      }
-		    });
+		    });*/
 		 JRadioButton hard = new JRadioButton("Hard");
 		 hard.setAlignmentX(CENTER_ALIGNMENT);
 		 hard.setAlignmentY(CENTER_ALIGNMENT);
 		 hard.setPreferredSize(new Dimension(100, 150));
 
-		 hard.addActionListener(new ActionListener() {
-		      public void actionPerformed(ActionEvent e)
+		 hard.addActionListener(e -> {
+			 difficulty = "hard";
+			 clicked = true;
+		 });
+		 
+		 /*hard.addActionListener(new ActionListener() {
+		     @Override 
+			 public void actionPerformed(ActionEvent e)
 		      {
 		        // display/center the jdialog when the button is pressed
 		        difficulty = "hard";
 		        clicked = true;
 		      }
-		    });
+		    });*/
 		 ButtonGroup group = new ButtonGroup();
 		    group.add(easy);
 		    group.add(medium);
@@ -128,12 +143,12 @@ public class DifficultyChange extends JPanel {
 	//Regular button
 	
 	//Button with an icon
-	public void addButton(String name, JPanel p, String imgURL){
+	public void addButton(String name, JPanel p){
 		 JButton button = new JButton(name);
 		 p.add(button);
 	}
 	
-	public class backgroundPanel extends JPanel
+	public class BackgroundPanel extends JPanel
 	{
 		@Override
 	    public void paintComponent(Graphics g)
